cb89ba748323dafec1a5fcd5f185b87c16608942
Roma <Roma@DESKTOP-VNC5LP4>
2024-01-24 10:48:43 +0200

    Add project files.

---
+++ urok/Program.cs
@@ -0,0 +1,87 @@
+﻿
+
+/*способи оголошення масиву
+* int[] Array;
+
+int size = 10;
+int[] Array2 = new int[size];
+
+int max = 0;
+int[] Array3 = { 1, -2, -1, -100, 1, 500, 5, 0, -10 };
+*/
+
+//int[] Array;
+
+//int size = 10;
+//int[] Array2 = new int[size];
+
+//int max = 0;
+//int[] Array3 = { 1, -2, -1, -100, 1, 500, 5, 0, -10 };
+////Console.WriteLine(Array3[0]);
+////Array3[0] = 5;
+////Console.WriteLine(Array3[0] );
+
+//for (int i = 0; i < 9; i++)
+//{
+//    Console.Write(Array3[i]+" "  );
+//}
+//Console.WriteLine();
+
+//for (int i = 0; i < 9; i++)
+//{
+//    if (Array3[i] > max) {
+//        max = Array3[i];
+//    }
+//}
+//Console.WriteLine( max);
+
+//int min = 0;
+
+
+//int max = 0;
+//int[] Array3 = { 5, -1, 9, -69, 37, 891,-998};
+//for (int i = 0; i < 6; i++)
+//{
+//    Console.Write(Array3[i] + " ");
+//}
+//Console.WriteLine();
+//for (int i = 0; i < 6; i++)
+//{
+//    if (Array3[i] < min)
+//    {
+//        min = Array3[i];
+//    }
+//}
+//Console.WriteLine(min);
+//int a = 0;
+//int b = 1;
+//Random rand = new Random();
+//int[] Array3 = {1, 1, 5, 6, 7, 80, 0, 0, 3, 1, 1, 1 };
+//for (int i = 0; i < Array3.Length; i++)
+//{
+//    if (Array3[i] == a || Array3[i] == b)
+//    {
+//        Array3[i] = rand.Next(0, 15);
+
+//    }
+//    Console.Write(Array3[i]+" ");
+//}
+//int u = 0;
+
+//int[] Array3 = {-5, -6, -1, 6, 7, 2, 5, 1, -8, 6, 2};
+//for (int i = 0; i < Array3.Length; i++)
+//{
+//    if (Array3[i] > 0)
+//    {
+//        Console.WriteLine(Array3[i]);
+//        break;
+//    }
+//}
+//for (int i = Array3.Length-1; i > 0; i--)
+//{
+//    if (Array3[i] < 0)
+//    {
+//        Console.WriteLine(Array3[i]);
+//        break;
+//    }
+//}
